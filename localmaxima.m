16e5f614818f5f82851ffd2dc7d7d5d0a971d240
Benjamin Seppke <benjamin.seppke@gmail.com>
2018-11-03 17:00:02 +0100

    added new vigra_c local minma/maxima functions.

--- localmaxima.m
+++ localmaxima.m
@@ -1,6 +1,16 @@
 function maxima_image = localmaxima(image, varargin)
     eight_connectivity = 1;
     if nargin > 1, eight_connectivity = varargin{1};end
+    marker = 1;
+    if nargin > 2, marker = varargin{2};end
+    threshold = -Inf('single');
+    if nargin > 3, threshold = varargin{3};end
+    allow_at_border = 0;
+    if nargin > 4, allow_at_border = varargin{4};end
+    allow_plateaus = 1;
+    if nargin > 5, allow_plateaus = varargin{5};end
+    plateau_epsilon = 0.001;
+    if nargin > 6, plateau_epsilon = varargin{6};end
 
     shape = size(image);
     w = shape(1);  
@@ -14,7 +24,7 @@ function maxima_image = localmaxima(image, varargin)
     maxima_image = zeros(w,h,b,'single');
     
     for i=1:b
-        maxima_image(:,:,i) = localmaxima_band(image(:,:,i),eight_connectivity);
+        maxima_image(:,:,i) = localmaxima_band(image(:,:,i),eight_connectivity, marker, threshold, allow_at_border, allow_plateaus, plateau_epsilon);
     end
     
 end
@@ -22,6 +32,16 @@ end
 function maxima_band = localmaxima_band(image_band, varargin)
     eight_connectivity = 1;
     if nargin > 1, eight_connectivity = varargin{1};end
+    marker = 1;
+    if nargin > 2, marker = varargin{2};end
+    threshold = -Inf('single');
+    if nargin > 3, threshold = varargin{3};end
+    allow_at_border = 0;
+    if nargin > 4, allow_at_border = varargin{4};end
+    allow_plateaus = 1;
+    if nargin > 5, allow_plateaus = varargin{5};end
+    plateau_epsilon = 0.001;
+    if nargin > 6, plateau_epsilon = varargin{6};end
     
     shape = size(image_band);
     w = shape(1);  
@@ -32,7 +52,7 @@ function maxima_band = localmaxima_band(image_band, varargin)
     maxima_band = zeros(w,h,'single');
     maxima_ptr = libpointer('singlePtr',maxima_band);
     
-    result = calllib('libvigra_c','vigra_localmaxima_c', ptr, maxima_ptr, w, h, eight_connectivity);
+    result = calllib('libvigra_c','vigra_localmaxima_c', ptr, maxima_ptr, w, h, eight_connectivity, marker, threshold, allow_at_border, allow_plateaus, plateau_epsilon);
     
     if ( result == 0 )
         maxima_band = maxima_ptr.Value;
--- localminima.m
+++ localminima.m
@@ -1,7 +1,17 @@
 function minima_image = localminima(image, varargin)
     eight_connectivity = 1;
     if nargin > 1, eight_connectivity = varargin{1};end
-
+    marker = 1;
+    if nargin > 2, marker = varargin{2};end
+    threshold = Inf('single');
+    if nargin > 3, threshold = varargin{3};end
+    allow_at_border = 0;
+    if nargin > 4, allow_at_border = varargin{4};end
+    allow_plateaus = 1;
+    if nargin > 5, allow_plateaus = varargin{5};end
+    plateau_epsilon = 0.001;
+    if nargin > 6, plateau_epsilon = varargin{6};end
+    
     shape = size(image);
     w = shape(1);  
     h = shape(2); 
@@ -14,7 +24,7 @@ function minima_image = localminima(image, varargin)
     minima_image = zeros(w,h,b,'single');
     
     for i=1:b
-        minima_image(:,:,i) = localminima_band(image(:,:,i), eight_connectivity);
+        minima_image(:,:,i) = localminima_band(image(:,:,i), eight_connectivity, marker, threshold, allow_at_border, allow_plateaus, plateau_epsilon);
     end
     
 end
@@ -22,6 +32,16 @@ end
 function minima_band = localminima_band(image_band, varargin)
     eight_connectivity = 1;
     if nargin > 1, eight_connectivity = varargin{1};end
+    marker = 1;
+    if nargin > 2, marker = varargin{2};end
+    threshold = Inf('single');
+    if nargin > 3, threshold = varargin{3};end
+    allow_at_border = 0;
+    if nargin > 4, allow_at_border = varargin{4};end
+    allow_plateaus = 1;
+    if nargin > 5, allow_plateaus = varargin{5};end
+    plateau_epsilon = 0.001;
+    if nargin > 6, plateau_epsilon = varargin{6};end
     
     shape = size(image_band);
     w = shape(1);  
@@ -32,7 +52,7 @@ function minima_band = localminima_band(image_band, varargin)
     minima_band = zeros(w,h,'single');
     minima_ptr = libpointer('singlePtr',minima_band);
     
-    result = calllib('libvigra_c','vigra_localminima_c', ptr, minima_ptr, w,h, eight_connectivity);
+    result = calllib('libvigra_c','vigra_localminima_c', ptr, minima_ptr, w,h, eight_connectivity, marker, threshold, allow_at_border, allow_plateaus, plateau_epsilon);
     
     if ( result == 0 )
         minima_band = minima_ptr.Value;
